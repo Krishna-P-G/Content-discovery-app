45c58f10cff1a211b425ff8b255cc866e9664bf9
Krishna.P.G <krishnamel39@gmail.com>
2023-09-15 10:02:57 +0530

    prj

---
+++ day_3/login_frontend/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+// import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Login_Register/login';
+import Forgot from './Forgot_password/forgot';
+
+function App() {
+	return (
+        <BrowserRouter>
+          <Routes>
+          <Route
+              path='/'
+              element=
+              {
+                <>
+                  <Login />
+                </>
+              }
+          />
+          <Route
+              path='/forgot'
+              element=
+              {
+                <>
+                  <Forgot />
+                </>
+              }
+          />
+          </Routes>
+        </BrowserRouter>
+  )
+  }
+  
+export default App;
